2ed4ce0ff2389b8e539e775f82a00789f8a40772
JR Oakes <jroakes@gmail.com>
2021-01-01 19:23:45 -0500

    update ga

--- src/forecastga/__init__.py
+++ src/forecastga/__init__.py
@@ -23,7 +23,7 @@ from forecastga.ensembles import (
     ensemble_doubled,
 )
 from forecastga.models import MODELS
-import googleanalytics as ga
+import forecastga.googleanalytics as ga
 
 pd.plotting.register_matplotlib_converters()
 warnings.filterwarnings("ignore")
